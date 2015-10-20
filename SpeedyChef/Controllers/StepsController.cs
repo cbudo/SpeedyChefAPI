f03bbad7a2d2f3392a9bcb47c34475940c142e88
Alia <robinsat@rose-hulman.edu>
2015-10-19 22:46:30 -0400

    Added Steps Controller

---
+++ SpeedyChef/Controllers/StepsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpeedyChef.Controllers
+{
+    public class StepsController : Controller
+    {
+        //
+        // GET: /Steps/
+        public List<string> Get()
+        {
+           // string mealId = ""; //TODO get from request params
+           // SpeedyChefDataContext SCDC = new SpeedyChefDataContext();
+            List<String> temp = new List<string>();
+            temp.Add("Step 1");
+            temp.Add("Step 2");
+            temp.Add("Step 3");
+            return temp;
+        }
+	}
+}
---
+++ SpeedyChefApi/Controllers/StepsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpeedyChefApi.Controllers
+{
+    public class StepsController : Controller
+    {
+        //
+        // GET: /Steps/
+        public List<string> Index()
+        {
+            List<string> temp = new List<string>();
+            temp.Add("Step 1");
+            temp.Add("Step 2");
+            temp.Add("Step 3");
+            return temp;
+        }
+	}
+}
