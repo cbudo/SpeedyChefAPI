3ea981df4c2af64ec549b17b78b9758eeef9e742
cbudo <budocf@rose-hulman.edu>
2015-10-20 13:33:39 -0400

    added working preferences controller

--- SpeedyChefApi/Controllers/PreferencesController.cs
+++ SpeedyChefApi/Controllers/PreferencesController.cs
@@ -17,9 +17,22 @@ namespace SpeedyChefApi.Controllers
         public JsonResult getUserProperties(string user)
         {
             JsonResult json = new JsonResult();
-            Preferences prefs = new Preferences();
+            Models.Member member = new Models.Member();
             SpeedyChefDataContext SCDC = new SpeedyChefDataContext();
-            json.Data = prefs;
+            member.memname = SCDC.Members.Where(m => m.Memname == user).Select(m => m.Memname).First();
+            member.allergens = new List<Allergen>();
+            foreach (var allergen in SCDC.Member_Allergens.Where(m => m.Memname == member.memname))
+            {
+                FoodItem item = new FoodItem { FoodName = allergen.Foodname };
+                member.allergens.Add(new Allergen { FoodItem = item });
+            }
+            member.groups = new List<Models.Member_Group>();
+            foreach (var group in SCDC.Member_Groups.Where(m => m.Groupadmin == member.memname))
+            {
+                member.groups.Add(new Models.Member_Group { Name = group.Groupname, adminName = member.memname });
+            }
+            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            json.Data = member;
             return json;
         }
     }
