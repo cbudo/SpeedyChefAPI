746e2a68a1e7980b83a2bac82cbc12d5a1cc60c0
Stephen Trotta <trottasn@rose-hulman.edu>
2015-10-20 18:14:15 -0400

    Compatability issues? Hopefully not.

---
+++ SpeedyChefApi/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
+using SpeedyChefApi;
+
+
+namespace SpeedyChefApi.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: /Search/
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "balls" };
+        }
+
+        // POST: /Search/
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: /Search/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: /Search/5
+        public void Delete(int id)
+        {
+        }
+
+        // SEARCH: /Search/
+        public string Search()
+        {
+            return "hello";
+        }
+    }
+}
