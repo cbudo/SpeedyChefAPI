a3991a4b9f8383723ed294d5d34bc4e9eccbc337
cbudo <budocf@rose-hulman.edu>
2015-10-20 13:43:57 -0400

    Revert "added working preferences controller"
    This reverts commit 3ea981df4c2af64ec549b17b78b9758eeef9e742.

--- SpeedyChefApi/Controllers/PreferencesController.cs
+++ SpeedyChefApi/Controllers/PreferencesController.cs
@@ -17,22 +17,9 @@ namespace SpeedyChefApi.Controllers
         public JsonResult getUserProperties(string user)
         {
             JsonResult json = new JsonResult();
-            Models.Member member = new Models.Member();
+            Preferences prefs = new Preferences();
             SpeedyChefDataContext SCDC = new SpeedyChefDataContext();
-            member.memname = SCDC.Members.Where(m => m.Memname == user).Select(m => m.Memname).First();
-            member.allergens = new List<Allergen>();
-            foreach (var allergen in SCDC.Member_Allergens.Where(m => m.Memname == member.memname))
-            {
-                FoodItem item = new FoodItem { FoodName = allergen.Foodname };
-                member.allergens.Add(new Allergen { FoodItem = item });
-            }
-            member.groups = new List<Models.Member_Group>();
-            foreach (var group in SCDC.Member_Groups.Where(m => m.Groupadmin == member.memname))
-            {
-                member.groups.Add(new Models.Member_Group { Name = group.Groupname, adminName = member.memname });
-            }
-            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
-            json.Data = member;
+            json.Data = prefs;
             return json;
         }
     }
--- SpeedyChefApi/Models/Preferences.cs
+++ SpeedyChefApi/Models/Preferences.cs
@@ -1,34 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpeedyChefApi.Models
 {
-    [DataContract]
-    public class Preferences
+    class Preferences
     {
-    }
-    public class Member
-    {
-        public string memname { get; set; }
-        public List<Member_Group> groups { get; set; }
-        public List<Allergen> allergens { get; set; }
-    }
-    public class Member_Group
-    {
-        public string adminName { get; set; }
-        public string Name { get; set; }
-        public List<string> memershipNames { get; set; }
-    }
-    public class Allergen
-    {
-        public FoodItem FoodItem { get; set; }
-    }
-    public class FoodItem
-    {
-        public string FoodName { get; set; }
+        public Member member { get; set; }
+        public List<Member_Group> group {get;set;}
+        public List<Tool> tools { get; set; }
+        public List<Oven> ovens { get; set; }
+        public List<Member_Allergen> allergens { get; set; }
     }
 }
