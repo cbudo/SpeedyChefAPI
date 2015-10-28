78d2db79a59370643d1f36e0cabc24947ff6d60a
Larry Gates <gateslm@rose-hulman.edu>
2015-10-28 01:44:22 -0400

    Slowly making progress on something
    Slowly working on getting some sort of API working, I will definitely
    will need help.

--- SpeedyChefApi/Controllers/CalendarScreenController.cs
+++ SpeedyChefApi/Controllers/CalendarScreenController.cs
@@ -4,36 +4,55 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
+using SpeedyChefApi;
 
 namespace SpeedyChefApi.Controllers
 {
-    public class CalendarScreenController : ApiController
+    public class CalendarScreenController : Controller
     {
-        // GET api/<controller>
+        // GET: /CalendarScreenController/
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
+        // POST: /CalendarScreenController/
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/<controller>/5
+        // PUT: /CalendarScreenController/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/<controller>/5
+        // DELETE: /CalendarScreenController/5
         public void Delete(int id)
         {
+        }
+
+        public ActionResult Calendar()
+        {
+            SpeedyChefDataContext context = new SpeedyChefDataContext();
+            Member temp = context.Members.First();
+            return Json(temp, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getUserCalendar(string user, DateTime date)
+        {
+            JsonResult json = new JsonResult();
+            Models.Member member = new Models.Member();
+            SpeedyChefDataContext scdc = new SpeedyChefDataContext();
+            member.memname = scdc.Members.Where(m => m.Memname == user).Select(m=>m.Memname).First();
+            // Don't exactly know what I am doing yet, but might as well get started.
+
+            return json;
         }
     }
 }
