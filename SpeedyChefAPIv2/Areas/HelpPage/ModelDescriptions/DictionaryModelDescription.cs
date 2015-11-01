7dfff973e117e1f3e6a6e663215c604d0caa2c5a
Stephen Trotta <trottasn@rose-hulman.edu>
2015-10-31 20:25:22 -0400

    Revert "now deployable"
    This reverts commit a48ec01a170273f555d0248646a39e777966d59b.

--- SpeedyChefAPIv2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SpeedyChefAPIv2.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SpeedyChefAPIv2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpeedyChefAPIv2.WebApiApplication" Language="C#" %>
